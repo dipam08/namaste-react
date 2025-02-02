21d1fe5ac8fc89d1a04787e6f1794ba22dc748fd
dipam08 <dipamdutta08@gmai.com>
2025-02-02 19:45:31 +0530

    episode 5 Top Rated Restaurants feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,30 @@
 import RestaurantCards from "./RestaurantCards";
 import resList from "../utils/mockswiggyData";
+import { useState } from "react";
 
 const Body = () => {
+  let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter Logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.card.card.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      {/* <div className="search">Search</div> */}
       <div className="restaurant-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCards
             key={restaurant.card.card.info.id}
             resData={restaurant}
